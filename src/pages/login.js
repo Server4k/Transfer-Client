6b515e1f062de44920d4c365074cabc136e3ca79
Server4k <servermc4k@gmail.com>
2024-11-14 22:14:08 +0530

    Update login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -12,7 +12,7 @@ function App() {
   }
   async function loginUser(event) {
     event.preventDefault()
-    const response = await fetch('http://localhost:1337/api/login', {
+    const response = await fetch('https://transfer-server.vercel.app:1337/api/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
